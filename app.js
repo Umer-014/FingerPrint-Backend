3bdb0e67e7c5609afa3b0a1c301224e2e8df2b40
Umer-014 <umer.riaz2810@gmail.com>
2024-12-26 23:13:23 +0500

    updated more

--- app.js
+++ app.js
@@ -28,5 +28,8 @@ app.get('/api', (req, res) => {
   });
   
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
